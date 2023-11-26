6cf7fa6f0f82c6683207e9934e9becebdd30049e
carlota1205 <104369950+carlota1205@users.noreply.github.com>
2023-11-26 15:16:49 -0300

    Delete TPEprueba directory

--- TPEprueba/Models/Model.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'config.php';
-abstract class Model{
-    protected $db;
-
-    function __construct(){
-        $this->db = new PDO("mysql:host=". DB_HOST .";dbname=". DB_NAME .";charset=utf8", DB_USER, "");
-    }
-}

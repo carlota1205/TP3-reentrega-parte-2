2df21b866203ae8a58555cd51386e0b050a6d684
carlota1205 <104369950+carlota1205@users.noreply.github.com>
2023-11-26 15:35:38 -0300

    Add files via upload

---
+++ TPEprueba/Models/ModelUsuario.admin.php
@@ -0,0 +1,18 @@
+<?php
+require_once 'Models/Model.php';
+
+class ModelUsuarioAdmin extends Model{
+
+    function __construct(){
+        parent::__construct();
+    }
+
+    public function login($user){
+        $query = $this->db->prepare("SELECT * FROM usuario_admin WHERE Nombre_Usuario = ?");
+        $query->execute([$user]);
+        $datos = $query->fetch(PDO::FETCH_OBJ);
+        return $datos;
+    }
+   
+    
+}
